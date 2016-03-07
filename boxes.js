03bef03803d3070f7ac55c140ef16157550ba398
Rick Budzak <pyroja@gmail.com>
2016-03-07 13:29:37 -0800

    Nearly done

--- boxes.js
+++ boxes.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+$(document).ready(function(){
+  console.log("hello world");
+
+  //Exercise 1
+  alert("All systems are go.");
+
+  // Exercise 2
+  // $('#secretBox').css('background-color', 'white');
+  // $('#secretBox').append('<h1> MY SECRET IS OUT</h1>');
+  // $('#row1').children().removeClass().addClass('box boxType3');
+  // $('#row4 div').last().css('display', 'none');
+  // $('.boxType1').css('background-color', 'white');
+  // $('#row2').children().slice(0,2).removeClass();
+  // $('.box').not('#secretBox').css('width','2px');
+
+  //Exercise 3
+  $('#container').click(function(e){
+    console.log(e.pageX + ", " + e.pageY);
+    return e.pageX + ", " + e.pageY;
+  });
+  $('.boxType1').append("<a href=\"http://galvanize.com\">Galvanize</a>");
+  $('a').click(function (e){
+    e.preventDefault();
+    alert("YOU SHALL NOT PASS!");
+  });
+
+});
